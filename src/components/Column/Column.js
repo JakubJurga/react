99f18eeb94c7e62e39d66a76070df5552fb9053f
Jakub Jurga <>
2021-06-07 11:25:18 +0100

    Add columns and cards via redux, zadanie 16.4

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-//import {settings} from '../../data/dataStore';
+import {settings} from '../../data/dataStore';
 import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 
 class Column extends React.Component {
@@ -16,6 +16,11 @@ class Column extends React.Component {
     title: PropTypes.string.isRequired,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   }
 
   /*addCard(title){
@@ -34,7 +39,7 @@ class Column extends React.Component {
   }*/
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}<span className={styles.icon}><Icon name={icon} /></span></h3>
@@ -43,10 +48,10 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*}
+
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div>*/}
+          <Creator text={settings.cardCreatorText} action={addCard}/>
+        </div>
 
       </section>
 
